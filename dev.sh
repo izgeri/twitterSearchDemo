dcc7c10391864b2ca910a3d5a1ec6350e50ae3ea
Geri Jennings <gerijennings@ssid-umass-vl924-172-30-24-151.wireless.umass.edu>
2018-03-17 14:28:27 -0400

    bug fix - docker-compose filename correction for dev.sh

--- dev.sh
+++ dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
 
 # point to the dev docker-compose file and start the containers
-summon docker-compose -f dev/docker-compose.yml up -d
+summon docker-compose -f docker-compose-dev.yml up -d
